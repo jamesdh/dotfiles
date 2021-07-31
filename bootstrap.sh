1264341335e08f430d82724bc992c4606ce37400
James Hardwick <jamesdh@gmail.com>
2021-07-31 08:58:27 -0500

    Clean up bootstrap, simplify

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,24 +9,27 @@ echo "Checking for mas..."
 echo "Preinstalling apps required for setup or requiring additional permissions prompts"
 export HOMEBREW_CASK_OPTS='--no-quarantine'; brew bundle --file=roles/osx/files/Brewfile.preprompt
 
-echo "Checking 1password initial login status..."
-eval $(make login.op)
-
-# echo "Checking App Store login status..."
-# # [[ ! `mas account 1> /dev/null` ]] && osascript -l JavaScript jxa/app_store.scpt 1> /dev/null && read -p "Please sign into the App Store to continue..."
-
-# echo "Checking Accessibility status..."
-# [[ ! `osascript -l JavaScript jxa/access_check.scpt` ]] && echo "Re-run script after making necessary permissions changes!" && exit 1
+echo "Checking App Store login status..."
+make login.appstore
 
 echo "Checking for vault password file..."
 if [[ ! -f ~/.ansible/dotfiles_vaultpass ]]; then 
+  eval $(make login.op)
   echo "Retrieving vault password"
   mkdir -p -m 0755 ~/.ansible
   touch ~/.ansible/dotfiles_vaultpass
   op get item "Ansible Vault - Dotfiles" --fields password > ~/.ansible/dotfiles_vaultpass
 fi
 
-# ansible-playbook --ask-become-pass --diff --skip-tags "postinstall" ansible.yml
-# ansible-playbook --ask-become-pass --diff --tags "postinstall" ansible.yml
-
 echo "Finished bootstrapping! Run \`make install\` to proceed."
+echo ""
+echo "To reduce the number of security prompts during install which can interrupt the install flow, first perform the following:"
+echo "Go to..."
+echo " - System Preferences"
+echo "   - Security & Privacy"
+echo "     - Privacy"
+echo "       - Accessibility"
+echo "         - Enable all shown applications"
+echo "       - Full Disk Access"
+echo "         - Enable Terminal + iTerm"
+echo ""
