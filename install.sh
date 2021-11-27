650d6dd17e10b42422275109428825ef24837dac
James Hardwick <jamesdh@gmail.com>
2021-11-27 12:58:09 -0600

    Add rosetta on initial run

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 xcode-select --install
+softwareupdate --install-rosetta
 mkdir -p ~/Projects/jamesdh/dotfiles 
 cd ~/Projects/jamesdh/dotfiles 
 git clone https://github.com/jamesdh/dotfiles.git . 
