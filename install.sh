5cfe8b691a2acfb8df90feccfa4f56cc72c5b595
James Hardwick <jamesdh@gmail.com>
2024-03-16 17:01:20 -0500

    Make installer more intelligent still

--- install.sh
+++ install.sh
@@ -7,6 +7,11 @@ xcode-select -p >& /dev/null || {
     echo "Select \"Install\" when prompted for Command Line Developer Tools"
     sleep 3
     osascript -e 'tell application "Install Command Line Developer Tools" to activate'
+    while ! pkgutil --pkg-info=com.apple.pkg.CLTools_Executables &>/dev/null; do
+        echo "Waiting for Xcode Command Line Tools to finish installing..."
+        sleep 5
+    done
+    echo "Xcode Command Line Tools are installed."
 }
 
 # If missing, install Rosetta 2
@@ -18,6 +23,7 @@ pkgutil --pkgs=com.apple.pkg.RosettaUpdateAuto >& /dev/null || {
 mkdir -p ~/Projects/jamesdh/dotfiles 
 cd ~/Projects/jamesdh/dotfiles 
 if [[ ! -d .git ]]; then
+    echo "Cloning dotfiles locally..."
     git clone https://github.com/jamesdh/dotfiles.git . 
 fi
 
